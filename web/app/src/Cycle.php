0747c0d99516f3fdf53601f4baa1c93323fcac83
z0r0v <3767082@mail.ru>
2021-09-04 09:35:47 +0300

    add get url

--- web/app/src/Cycle.php
+++ web/app/src/Cycle.php
@@ -16,27 +16,43 @@ class Cycle
      * Gets the name of the application.
      */
 
+    public $colors = array('red', 'orange', 'yellow', 'green', 'violet');
 
     public function getCycle()
     {
-        $colors = $this->getColors();
-        for ($i = 0; $i <= sizeof($colors); $i++) {
-            echo $colors[$i] . " ";
+        for ($i = 0; $i <= sizeof($this->colors); $i++) {
+            echo $this->colors[$i] . " ";
         }
     }
 
-    public function getColors()
+
+    public function getTemplateColors()
     {
-        $colors = array('red', 'orange', 'yellow', 'green', 'violet');
-
-        return $colors;
-    }
-
-    public function getTemplateColors() {
-        $colors = $this->getColors();
-
-        foreach ($colors as $key => $value) {
+        foreach ($this->colors as $key => $value) {
             echo '<div style="background:' . $value . '; width: 50px; height: 50px;"><span>' . $key . '</span> </div>';
         }
+    }
+
+    public function getWhile()
+    {
+        $i = 0;
+
+        while ($i <= 10) {
+            $a = $i * 7;
+            echo $a . " ";
+            $i++;
+        }
+
+        echo '</br></br>';
+
+        $j = 0;
+
+        while ($j <= count($this->colors)) {
+
+            echo $this->colors[$j] . "</br>";
+
+            $j++;
+        }
+
     }
 }
--- web/public/index.php
+++ web/public/index.php
@@ -7,6 +7,7 @@ $start = new App\Acme\Start();
 $isUserlogin = new App\Acme\Isuserlogin();
 $operators = new App\Acme\Operators();
 $cycle = new App\Acme\Cycle();
+$get = new App\Acme\Get();
 
 ?>
 <!DOCTYPE html>
@@ -16,21 +17,44 @@ $cycle = new App\Acme\Cycle();
     <title>Docker <?php echo $foo->getName(); ?></title>
 </head>
 <body>
-    <p>Get name:</p>
-    <h1>Docker <?php echo $foo->getName(); ?></h1>
-    <p>Start:</p>
-    <div><?php echo $start->getVariableArray(); ?></div>
-    <p>Is user login:</p>
-    <div><?php echo $isUserlogin->isUserLogin(); ?></div>
+<!--    <p>Get name:</p>-->
+<!--    <h1>Docker --><?php //echo $foo->getName(); ?><!--</h1>-->
+<!---->
+<!--    <p>Start:</p>-->
+<!--    <div>--><?php //echo $start->getVariableArray(); ?><!--</div>-->
+<!--    <p>Is user login:</p>-->
+<!--    <div>--><?php //echo $isUserlogin->isUserLogin(); ?><!--</div>-->
+<!---->
+<!--    <p>Operators:</p>-->
+<!--    <div>--><?//= $operators->getOperators(); ?><!--</div>-->
+<!---->
+<!--    <p>Cycle:</p>-->
+<!--    <div>--><?//= $cycle->getCycle(); ?><!--</div>-->
+<!--    <div>--><?//= $cycle->getTemplateColors() ?><!--</div>-->
+<!--    <div>--><?//= $cycle->getWhile(); ?><!--</div>-->
 
-    <p>Operators:</p>
-    <div><?= $operators->getOperators(); ?></div>
-
-    <p>Cycle:</p>
-    <div><?= $cycle->getCycle(); ?></div>
+    <p>Get URL:</p>
+    <div><?= $get->getGet(); ?></div>
     <div>
-        <?= $cycle->getTemplateColors() ?>
+        <p>Input your name</p>
+        <form>
+            <input type="text" name="userName">
+            <input type="submit" name="">
+        </form>
+
+        <p><?= $get->getUrl(); ?></p>
+
+        <p>Input your number</p>
+        <form>
+            <input type="text" name="numberEvenOdd">
+            <input type="submit" name="">
+        </form>
+
+        <p><?= $get->СheckNumberOddEven(); ?></p>
+
+
     </div>
+
 </body>
 </html>
 
